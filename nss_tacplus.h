f7bfa9f4fbc7357a14cc34c9ab53f98a5a0f9da0
Dave Olson <olson@cumulusnetworks.com>
2016-10-21 16:01:03 -0700

    Updated COPYING, corrected copyrights
    Also fixed some whitespace at end of lines

--- nss_tacplus.h
+++ nss_tacplus.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2014, 2015, 2016 Cumulus Networks, Inc
- * 
+ * Copyright (C) 2014, 2015, 2016 Cumulus Networks, Inc.  All rights reserved.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
