86f2c6782d0e59e633ec7ae44132fc4f46a32aaa
Dave Olson <olson@cumulusnetworks.com>
2017-05-25 12:58:38 -0700

    When re-using a secret key for multiple servers, strdup, don't copy
    Otherwise when reconfiguring when we detect a changed config
    file, we'll double free the strdup'ed memory.

--- nss_tacplus.c
+++ nss_tacplus.c
@@ -260,7 +260,8 @@ static int nss_tacplus_config(int *errnop, const char *cfile, int top)
                         tac_srv[tac_srv_no].addr = server;
                         /* use current key, if our index not yet set */
                         if(tac_key_no && !tac_srv[tac_srv_no].key)
-                            tac_srv[tac_srv_no].key = tac_srv[tac_key_no-1].key;
+                            tac_srv[tac_srv_no].key =
+                                strdup(tac_srv[tac_key_no-1].key);
                         tac_srv_no++;
                     }
                 }
